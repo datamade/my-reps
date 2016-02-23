03681c0e9196ce856d01e57a7619611b6fbf74fb
Derek Eder <derek.eder@gmail.com>
2016-02-23 17:32:53 -0600

    sorting candidates properly, added placeholder image, general styling and formatting

--- js/lookup_tool.js
+++ js/lookup_tool.js
@@ -4,9 +4,9 @@ var INFO_API = 'https://www.googleapis.com/civicinfo/v2/representatives';
 
 // parsing out division IDs
 var federal_pattern = "ocd-division/country:us";
-
-// this is still broken
-var state_pattern = /ocd-division\/country:us\/state:(\D{2}$)(\/sldl|\/sldu)?/;
+var state_pattern = /ocd-division\/country:us\/state:(\D{2}$)/;
+var cd_pattern = /ocd-division\/country:us\/state:(\D{2})\/cd:/;
+var sl_pattern = /ocd-division\/country:us\/state:(\D{2})\/(sldl:|sldu:)/;
 var county_pattern = /ocd-division\/country:us\/state:\D{2}\/county:\D+/;
 
 var federal_offices = ['United States Senate', 'United States House of Representatives']
@@ -24,6 +24,10 @@ var social_link_lookup = {
     'Twitter': 'https://twitter.com/',
     'GooglePlus': 'https://plus.google.com/'
 };
+
+var selected_state = '';
+var selected_county = '';
+var selected_local = '';
 
 function addressSearch() {
     var address = $('#address').val();
@@ -48,7 +52,7 @@ function addressSearch() {
         // console.log(data);
 
         $.each(divisions, function(division_id, division){
-            // console.log(division_id);
+            // console.log(division.name);
             if (typeof division.officeIndices !== 'undefined'){
                 
                 $.each(division.officeIndices, function(i, office){
@@ -66,16 +70,18 @@ function addressSearch() {
                         };
 
 
-                        console.log(officials[official])
+                        // console.log(officials[official])
                         var person = officials[official];
                         info['person'] = person;
 
                         if (typeof person.channels !== 'undefined'){
                             var channels = [];
                             $.each(person.channels, function(i, channel){
-                                channel['icon'] = social_icon_lookup[channel.type];
-                                channel['link'] = social_link_lookup[channel.type] + channel['id'];
-                                channels.push(channel);
+                                if (channel.type != 'GooglePlus' && channel.type != 'YouTube') {
+                                    channel['icon'] = social_icon_lookup[channel.type];
+                                    channel['link'] = social_link_lookup[channel.type] + channel['id'];
+                                    channels.push(channel);
+                                }
                             });
                             info['channels'] = channels;
                         }
@@ -89,11 +95,11 @@ function addressSearch() {
                             info['emails'] = person.emails;
                         }
 
-                        if(division_id == federal_pattern) {
+                        if(checkFederal(division_id, office_name)) {
                             federal_people.push(info);
-                        } else if (state_pattern.test(division_id)) {
+                        } else if (checkState(division_id)) {
                             state_people.push(info);
-                        } else if (county_pattern.test(division_id)){
+                        } else if (checkCounty(division_id)){
                             county_people.push(info);
                         } else {
                             local_people.push(info);
@@ -108,17 +114,35 @@ function addressSearch() {
         var template = new EJS({'text': $('#tableGuts').html()});
         
         $('#federal-results tbody').append(template.render({people: federal_people}));
-        $('#federal-results').show();
-
         $('#state-results tbody').append(template.render({people: state_people}));
-        $('#state-results').show();
-
         $('#county-results tbody').append(template.render({people: county_people}));
-        $('#county-results').show();
-
         $('#local-results tbody').append(template.render({people: local_people}));
-        $('#local-results').show();
+        $('#response-container').show();
     });
+}
+
+function checkFederal(division_id, office_name) {
+    if( division_id == federal_pattern || 
+        cd_pattern.test(division_id) ||
+        federal_offices.indexOf(office_name.name) >= 0)
+        return true;
+    else
+        return false; 
+}
+
+function checkState(division_id){
+    if( state_pattern.test(division_id) ||
+        sl_pattern.test(division_id))
+        return true;
+    else
+        return false; 
+}
+
+function checkCounty(division_id){
+    if( county_pattern.test(division_id))
+        return true;
+    else
+        return false; 
 }
 
 //converts a slug or query string in to readable text
