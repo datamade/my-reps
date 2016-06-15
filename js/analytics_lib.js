a727284c835b2713eb5a928f6b903cbd8f4116d8
Derek Eder <derek.eder@gmail.com>
2016-06-15 17:00:12 -0500

    updated analytics code

--- js/analytics_lib.js
+++ js/analytics_lib.js
@@ -10,7 +10,7 @@
  *
  */
 
-var analyticsTrackingCode = 'UA-39054699-2'; //enter your tracking code here
+var analyticsTrackingCode = 'UA-79410311-1'; //enter your tracking code here
 
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', analyticsTrackingCode]);
